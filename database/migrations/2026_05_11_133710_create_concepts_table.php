d965c05b58dd332fb0108db9166d8917d45a86fa
wissalaitihya <wissalaitihya@gmail.com>
2026-05-11 15:14:51 +0100

    install breeze

---
+++ database/migrations/2026_05_11_133710_create_concepts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('concepts', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('domain_id')
+              ->constrained()
+              ->cascadeOnDelete();
+        $table->string('title');
+        $table->text('explanation');
+        $table->enum('difficulty', ['junior', 'mid', 'senior'])->default('junior');
+        $table->enum('status', ['to_review', 'in_progress', 'mastered'])->default('to_review');
+        $table->softDeletes();
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('concepts');
+    }
+};
---
+++ database/migrations/2026_05_11_133716_create_generated_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('generated_questions', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('concept_id')
+              ->constrained()
+              ->cascadeOnDelete();
+        $table->json('questions');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('generated_questions');
+    }
+};
